ed51beab74c1281934ebf0e983410c53dd1ca67a
aamendez2 <mendeand13@gmail.com>
2016-02-03 12:09:49 -0600

    Module 2 Activity
    help

--- js/main.js
+++ js/main.js
@@ -5,50 +5,143 @@
 // mydiv.innerHTML = "Hello World";
 // };
 // windows.onload = myFunc	();
-function initialize()
+
+// function initialize(){
+//     cities();
+// };
+
+
+// function cities(){
+    
+//     var cityPop = [
+//         { 
+//             city: "Madison",
+//             population: 243344
+//         },
+//         {
+//             city: "Sauk City",
+//             population: 3472
+//         },
+//         {
+//             city: "New York",
+//             population: 8406000
+//         },
+//         {
+//             city: "Tokyo",
+//             population: 13230000
+//         }
+//     ];
+
+    
+//     $("#mydiv").append("<table>");
+    
+//     $("table").append("<tr>");
+    
+//     $("tr").append("<th>City</th><th>Population</th>");
+
+//     for (var i = 0; i < cityPop.length; i++){
+//         //assign longer html strings to a variable
+//         var rowHtml = "<tr><td>" + cityPop[i].city + "</td><td>" + cityPop[i].population + "</td></tr>";
+//         //add the row's html string to the table
+//         $("table").append(rowHtml);
+//     };
+// };
+
+// $(document).ready(initialize);
+
+
+///////////////////////////////////////////////////////////////////////
+//////////////////// /   /   /   /  /  /  /  /////////////////////////
+/////////////////// /   /   /   /  /  /  /  /////////////////////////
+////////////////// /   /   /   /  /  /  /  /////////////////////////
+///////////////// /   /   /   /  /  /  /  /////////////////////////
+//////////////// /   /   /   /  /  /  /  /////////////////////////
+/////////////// /   /   /   /  /  /  /  /////////////////////////
+////////////// /   /   /   /  /  /  /  /////////////////////////
+///////////// /   /   /   /  /  /  /  /////////////////////////
+//////////// /   /   /   /  /  /  /  /////////////////////////
+/////////// /   /   /   /  /  /  /  /////////////////////////
+////////// /   /   /   /  /  /  /  /////////////////////////
+///////// /   /   /   /  /  /  /  /////////////////////////
+//////// /   /   /   /  /  /  /  /////////////////////////
+/////// /   /   /   /  /  /  /  /////////////////////////
+////// /   /   /   /  /  /  /  /////////////////////////
+///// /   /   /   /  /  /  /  /////////////////////////
+//// /   /   /   /  /  /  /  /////////////////////////
+/// /   /   /   /  /  /  /  /////////////////////////
+// /   /   /   /  /  /  /  /////////////////////////
+///////////////////////////////////////////////////
+
+function addColumns(cityPop)
 {
-	cities();
-};
+    
+    $('tr').each(function(i)
+    {
 
-function cities()
-//two arrays for pop and city
-{
-	var cityPop = [
-	{ 
-		city: 'Madison',
-		population: 233209
-	},
-	{
-		city: 'Milwaukee',
-		population: 594833
-	},
-	{
-		city: 'Green Bay',
-		population: 104057
-	},
-	{
-		city: 'Superior',
-		population: 27244
-	}
-}
-];
+    	if (i == 0)
+    	{
 
-$("mydiv").append("<table>");
+    		$(this).apend('<th>City Size</th>');
+    	} 
+    	else 
+    	{
 
-$("table").append("<tr>");
+    		var citySize;
 
-$("tr").append("<th>City</th><th>Population</th>");
- $("tr").append("<th>City</th><th>Population</th>");
+    		if (cityPop[i-1].population < 100000)
+    		{
+    			citySize = 'Small';
 
-    //loop to add a new row for each city
-    for (var i = 0; i < cityPop.length; i++){
-        //assign longer html strings to a variable
-        var rowHtml = "<tr><td>" + cityPop[i].city + "</td><td>" + cityPop[i].population + "</td></tr>";
-        //add the row's html string to the table
-        $("table").append(rowHtml);
+    		} 
+    		else if (cityPop[i-1].population < 500000)
+    		{
+    			citysize = 'Medium';
+
+    		} 
+    		else
+    		{
+    			citySize = 'Large';
+    		}
+
+    		$this.append('<td' + citySize + '</td>');
+    	};
     };
 };
 
-//call the initialize function when the document has loaded
-$(document).ready(initialize);
+function addEvents()
+{
 
+	$('#table').mouseover(function()
+	{
+		
+		var color = "rgb(";
+
+		for (var i=0; i<3; i++)
+		{
+
+			var random = Math.round(Math.random() * 255);
+
+			color += "random";
+
+			if (i<2)
+			{
+				color += ",";
+			
+			}
+			 else 
+			{
+				color += ")";
+			}
+		};
+
+		$(this).css('color', color);
+	};
+
+	function clickMe()
+	{
+
+		alert('Hey, you clicked me!');
+	};
+
+	$('table').on('click', clickMe);
+};
